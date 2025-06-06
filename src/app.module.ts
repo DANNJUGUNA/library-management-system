801a6df595f7a1a2d5deee9bce83f2e9dfa12a16
DANNJUGUNA <ndan30071@gmail.com>
2025-06-06 22:46:28 +0300

    books module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksController } from './books/books.controller';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
+  imports: [BooksModule],
   controllers: [AppController, BooksController],
   providers: [AppService],
 })
