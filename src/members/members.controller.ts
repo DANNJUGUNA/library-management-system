5f5d00e1b5fd74e818dda1798471305ff64dd51c
DANNJUGUNA <ndan30071@gmail.com>
2025-06-08 14:30:11 +0300

    worked on members controller

--- src/members/members.controller.ts
+++ src/members/members.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
+import { MembersService } from './members.service';
+import { CreateMemberDto } from './dto/create-member.dto';
+import { UpdateMemberDto } from './dto/update-member.dto';
 
 @Controller('members')
-export class MembersController {}
+export class MembersController {
+  constructor(private readonly membersService: MembersService) {}
+  @Get()
+  getAllMember() {
+    return this.membersService.getAllMembers();
+  }
+  @Get(':id')
+  getMemberById(@Param('id', ParseIntPipe) id: number) {
+    return this.membersService.getMemberById(id);
+  }
+  @Post()
+  addMember(@Body(ValidationPipe) createMemberDto: CreateMemberDto) {
+    return this.membersService.addMember(createMemberDto);
+  }
+  @Patch(':id')
+  updateMember(
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe) updateMemberDto: UpdateMemberDto,
+  ) {
+    return this.membersService.updateMember(id, updateMemberDto);
+  }
+  @Delete(':id')
+  deleteMember(@Param('id', ParseIntPipe) id: number) {
+    return this.membersService.deleteMember(id);
+  }
+}
--- src/members/members.service.ts
+++ src/members/members.service.ts
@@ -1,4 +1,69 @@
 import { Injectable } from '@nestjs/common';
+import { CreateMemberDto } from './dto/create-member.dto';
+import { UpdateMemberDto } from './dto/update-member.dto';
 
 @Injectable()
-export class MembersService {}
+export class MembersService {
+  private Members = [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'jdoe@info.com',
+      book: 'Naturing Self Peace',
+    },
+    {
+      id: 2,
+      name: 'Peter Keans',
+      email: 'pk@info.com',
+    },
+    {
+      id: 3,
+      name: 'Ethan Leak',
+      email: 'el@info.com',
+      book: 'The Seal',
+    },
+    {
+      id: 4,
+      name: 'Jennifer Perez',
+      email: 'jp@info.com',
+      book: 'The Seal',
+    },
+    {
+      id: 5,
+      name: 'James Dean ',
+      email: 'jd@info.com',
+      book: 'The Daring King',
+    },
+  ];
+  getAllMembers() {
+    return this.Members;
+  }
+
+  getMemberById(id: number) {
+    const member = this.Members.find((member) => member.id === id);
+    return member;
+  }
+  addMember(createMemberDto: CreateMemberDto) {
+    const membersByHighestId = [...this.Members].sort((a, b) => b.id - a.id);
+    const newMember = {
+      id: membersByHighestId[0].id + 1,
+      ...createMemberDto,
+    };
+    this.Members.push(newMember);
+    return newMember;
+  }
+  updateMember(id: number, updateMemberDto: UpdateMemberDto) {
+    this.Members = this.Members.map((member) => {
+      if (member.id) {
+        return { ...member, ...updateMemberDto };
+      }
+      return member;
+    });
+    return this.getMemberById(id);
+  }
+  deleteMember(id: number) {
+    const deletedMember = this.getMemberById(id);
+    this.Members = this.Members.filter((member) => member.id !== id);
+    return deletedMember;
+  }
+}
