beedb0ac18f8299b3e563592bcb5b3efedb0a481
DANNJUGUNA <ndan30071@gmail.com>
2025-06-08 11:25:10 +0300

    generated the module, service and controller for the members

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { BooksController } from './books/books.controller';
 import { BooksModule } from './books/books.module';
 import { BooksService } from './books/books.service';
+import { MembersController } from './members/members.controller';
+import { MembersService } from './members/members.service';
+import { MembersModule } from './members/members.module';
 
 @Module({
-  imports: [BooksModule],
-  controllers: [AppController, BooksController],
-  providers: [AppService,BooksService],
+  imports: [BooksModule, MembersModule],
+  controllers: [AppController, BooksController, MembersController],
+  providers: [AppService,BooksService, MembersService],
 })
 export class AppModule {}
