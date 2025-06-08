24a77718c4285e5708ca60ccef4836aeecea5522
DANNJUGUNA <ndan30071@gmail.com>
2025-06-08 10:50:48 +0300

    added validators

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,5 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateBookDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+  @IsString()
   author: string;
+  @IsString()
   Year_of_publication: string;
 }
